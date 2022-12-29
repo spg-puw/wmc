e07cf7e0e68a2e449730d49b3316a9e783696858
spg-puw <117909782+spg-puw@users.noreply.github.com>
2022-12-29 20:10:46 +0100

    helpers

---
+++ 03_express/05_api_db/lib/helpers.js
@@ -0,0 +1,52 @@
+const entities = require('html-entities');
+const createHash = require('node:crypto').createHash;
+const jwt = require("jsonwebtoken");
+
+function omitPassword(user) {
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
+
+module.exports.omit = (obj, key) => { const { [key]: ignore, ...rest } = obj; return rest; }
+
+module.exports.hash = (cleartext) => {
+    const hashIdentifier = '$6$';
+    const salt = 'ohH6xooL';
+    return hashIdentifier + createHash('sha512').update(salt + cleartext).digest('hex');
+}
+
+module.exports.generateJwtToken = (userId, permissions) => {
+    var payload = {
+        userId: userId,
+        //scope: permissions.join(" "),
+        permissions: permissions,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '28d', audience: process.env.JWT_AUD, issuer: process.env.JWT_ISS });
+    return token;
+}
+
+//get token from Authorization header or query parameter ?_jwt=
+module.exports.getJwtTokenFromReq = (req) => {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query._jwt) {
+        return req.query._jwt;
+    }
+    return null;
+}
+
+//extend String.prototype.nl2br
+if (!global.String.prototype.nl2br) {
+    global.String.prototype.nl2br = function (str) {
+        var s = str || this;
+        return s.replace(/\r|\n|\r\n/g, '<br />')
+    }
+}
+
+//extend String.prototype.htmlspecialchars
+if (!global.String.prototype.htmlspecialchars) {
+    global.String.prototype.htmlspecialchars = function (str) {
+        var s = str || this;
+        return entities.encode(s);
+    }
+}
