636c0cdf2adbf9965b5791f14be84290a2459522
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-09-10 08:47:26 +0200

    add more console demos

--- 01_node/01_node_basics/index.js
+++ 01_node/01_node_basics/index.js
@@ -7,6 +7,35 @@ console.log(`Das Ergebnis ist ${ergebnis1} bzw. man könnte das auch direkt brec
 
 console.log(`Ausgabe direkt mit console.log(...):`);
 console.log(meinJsonObjekt);
-console.log('');
+console.log(''); // leere Zeile
+
+console.log(`Nur das Alter der Person: ${meinJsonObjekt.age}\n`); // auch zusätzlicher Zeilenumbruch mit \n
+
+const { address: addressOnly, ...withoutAddress } = meinJsonObjekt;
+
+console.log(`Nur die Personendaten ohne Adresse:`);
+console.table(withoutAddress);
+
+console.log(`\nNur die Adressdaten:`);
+console.table(addressOnly);
+
+console.group("\nGruppe");
+console.log(`Eintrag 1`);
+console.log(`Eintrag 2`);
+console.log(`Eintrag 3`);
+console.log(`Eintrag 4`);
+console.groupEnd();
+
+console.log(`wieder ein normaler Eintrag\n`);
+
+function innen() {
+    console.log(`das ist innen`);
+    console.trace();
+}
+
+function aussen() {
+    console.log(`das ist aussen`);
+    innen();
+}
 
 console.log(`Nur das Alter der Person: ${meinJsonObjekt.age}`);
